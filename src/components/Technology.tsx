4c509811a5d1c20751c56167a8e8f23d1b30e216
Nujhat Tarannum <nujhatarannum24@gmail.com>
2026-09-23 21:43:39 +0600

    added remove functionality, Add functionality and completed the stack

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use,useState } from "react";
 import type { ITechnology } from "../types/techtype";
 
 interface ITechnologyProps{
@@ -7,10 +7,38 @@ interface ITechnologyProps{
 
 const Technology = ({technologiesPromise}: ITechnologyProps) => {
        console.log(technologiesPromise,"technologiesPromise");
+          
+    {/* for count array*/}
+      const [stack, setStack] = useState <ITechnology[]> ([]);
 
 const data = use(technologiesPromise);
          console.log(data ,"data");
+      
+         const handleAddToStack = (item: ITechnology) => {
+    const alreadyExists = stack.some(
+        (stackItem) => stackItem.id === item.id
+    );
 
+    if (alreadyExists) {
+        alert("This technology is already in your stack!");
+        return;
+    }
+
+    setStack([...stack, item]);
+};
+
+        {/* for removing item*/}
+     
+    const handleRemoveFromStack = (id: string) => {
+        setStack(stack.filter((item) => item.id !== id));
+    };
+
+         {/* for removing  All item*/}
+         
+       const handleRemoveAll = () => {
+           setStack([]);
+       };
+       
     return (
     <section className=" container mx-auto py-4 px-20">
             <h2 className="text-4xl font-bold mb-2">Explore the <span className=" text-fuchsia-500">Technologies</span> </h2>
@@ -20,26 +48,47 @@ const data = use(technologiesPromise);
           <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
 
               <div className="lg:col-span-3 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-6">
-                { data.map( (item) => {
+                { 
+                data.map( (item) => {
+                     const isAdded = stack.some( (stackItem) => stackItem.id === item.id );
                        return (
                         <div className="flex flex-col justify-between gap-3 rounded-2xl border border-slate-300 bg-white p-4 shadow-xs transition-shadow hover:shadow-md" >
                         <div >
                              <div className="flex justify-between items-center mb-4">
                                    <img src={item.icon} alt = {item.name} className="w-8 h-8"></img>
-                                <h6 className="text-md text-blue-400">{item.badge}</h6>
+                             {item.badge && ( <span className={`rounded-full px-3 py-1 text-xs font-medium ${item.badgeColor}`}>
+                                   {item.badge}
+                                 </span>
+                                )}
                               
                             </div>
                                <h2 className="  text-slate-950 text-2xl font-bold mb-2">{item.name}</h2>
                                <p className="text-md text-gray-500 " >{item.description}</p>
                         </div>
 
-                          <div className="flex items-center justify-between ">
-                            <span className="text-md text-gray-500">{item.category}</span>
-                             <span className="text-md text-gray-500">{item.difficulty}</span>
-                              <span>{item.rating}</span>
-                          </div>    
+                         <div className="flex items-center justify-between gap-2 text-xs">
+  
+                         <span className="shrink-0 rounded-lg bg-slate-100/80 px-2.5 py-1 font-medium text-slate-700">
+                                {item.category}
+                          </span>
 
-                              <button className=" bg-slate-950 text-xl text-gray-100 border rounded-2xl px-10 py-4" >{item.buttonText}</button>
+
+                         <span className="truncate text-center text-slate-500">
+                                      {item.difficulty}
+                           </span>
+
+ 
+                              <span className="flex shrink-0 items-center gap-1 font-bold text-gray-700">
+                                           <span className="text-amber-400">★</span>
+                                                {item.rating}
+                                                     </span>
+                              </div>
+
+                         <button onClick={() => handleAddToStack(item)} disabled={isAdded}
+                             className={`text-xl border rounded-2xl px-10 py-4 
+                                ${ isAdded ? "bg-gray-400 text-white cursor-not-allowed" : "bg-slate-950 text-gray-100" }`}>
+                            {isAdded ? "✓ Added to Stack" : "Add to Stack"}
+                        </button>
 
                         </div>
                     
@@ -47,12 +96,77 @@ const data = use(technologiesPromise);
                    })
                        }
               </div>
+                  {/* your stack part */}
 
-               <div className=" lg:col-span-1 lg:sticky rounded-2xl border border-slate-300 bg-white p-4 shadow-xs transition-shadow hover:shadow-md ">
-                <h2 className="text-xl font-bold ">Your Stack</h2>
-                <p className="text-md text-gray-500"> no technologies selected</p>
-                <button></button>
-              </div>
+               <div className="lg:col-span-1 lg:sticky lg:top-24 self-start rounded-2xl border border-slate-300 bg-white p-4 shadow-xs">
+
+    <h2 className="text-xl font-bold">
+        Your Stack
+    </h2>
+
+
+    {stack.length === 0 ? (
+       
+       <div >
+         <p className="text-md text-gray-500 mb-4">
+        No Technologies Selected yet
+    </p>
+            <p className=" mt-5 flex h-20 items-center justify-center rounded-2xl border border-dashed border-slate-300text-sm text-slate-400">
+                Your stack is empty.
+            </p>
+        </div>
+
+    ) : (
+
+        <>
+            <div className="flex flex-col gap-3">
+                <p className="text-md text-gray-500 mb-4">
+        {stack.length} Technology Selected
+    </p>
+                {stack.map((item) => (
+
+                    <div
+                        key={item.id}
+                        className="flex items-center justify-between border rounded-xl p-3  border-gray-200 shadow "
+                    >
+
+                        <div className="flex items-center gap-3">
+
+                            <img src = {item.icon} alt={item.name} className="w-8 h-8"  />
+
+                            <div>
+                                <h3 className=" text-md font-semibold">
+                                    {item.name}
+                                </h3>
+
+                                <p className="text-sm text-gray-500">
+                                    {item.category}
+                                </p>
+                            </div>
+
+                        </div>
+
+                        <button onClick={() => handleRemoveFromStack(item.id)}
+                            className="text-gray-500 font-bold">
+                            ✕
+                        </button>
+
+                    </div>
+
+                ))}
+
+            </div>
+
+            <button
+                onClick={handleRemoveAll}
+                className="w-full mt-16 border border-red-500 font-semibold rounded-xl bg-gray-50 px-4 py-3 text-red-500">
+                Remove All
+            </button>
+        </>
+
+    )}
+
+</div>
                    
           </div>
     </section>
--- src/types/techtype.ts
+++ src/types/techtype.ts
@@ -3,6 +3,7 @@ export interface ITechnology {
     id: string;
   icon: string;
   badge: string | null; // Nullable for cards without a top badge (like Next.js)
+  badgeColor?: string;
   name: string;
   description: string;
   category: string;
